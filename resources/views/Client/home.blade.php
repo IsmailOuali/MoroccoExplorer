15acabe0d866571b53c8f7558c163b4fc339f7ad
unknown <ismail.ouali@ump.ac.ma>
2024-04-07 15:33:17 +0000

    home

--- resources/views/Client/home.blade.php
+++ resources/views/Client/home.blade.php
@@ -9,11 +9,35 @@
         <title>Morocco Explorer</title>
     </head>
     <body class="bg-black">
-        <div class="object-top">
-            <img src="/images/bg-home.png" class="w-full">
-        </div>
-        <div class="w-full h-screen bg-[url('/images/bg-home1.png')] bg-cover">   
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo nostrum quos, nulla ea velit odit ipsam repellendus qui sit, repudiandae nobis dignissimos temporibus doloribus provident dolor! Vitae amet ut mollitia.</p>
-        </div>
+        <header>
+            <div class="object-top">
+                <img src="/images/bg-home.png" class="w-full">
+            </div>
+            <div class="w-full flex flex-col items-center justify-center h-screen bg-[url('/images/bg-home1.png')] bg-cover">   
+                <p class="pt-32 text-amber-600 font-bold flex items-center justify-center text-8xl">Discover<br>Moroccan Vibes</p>
+                <button type="button" class="w-28 mt-8 text-gray-900 font-bold bg-gradient-to-r from-amber-200 via-amber-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">More</button>
+            </div>
+        </header>
+
+        <section class="bg-amber-800">
+            <div class="flex items-center justify-center pt-8">
+                <p class="text-3xl text-white font-bold">A discuter</p>
+            </div>
+            <div class="w-1/2 mt-8 flex flex-col items-center">
+                <div class="mt-4 w-96">
+                    <img src="/images/logo.png" class="h-32 ">
+                </div>
+                <div class="mt-4 w-64">
+                    <img src="/images/logo.png" alt="">
+                </div>
+                <div class="mt-4 w-64">
+                    <img src="/images/logo.png" alt="">
+                </div>
+                <div class="mt-4 w-64">
+                    <img src="/images/logo.png" alt="">
+                </div>
+            </div>
+        </section>
+
     </body>
 </html>

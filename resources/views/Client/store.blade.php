9f06fefb19cfd0b0ffcf9df0ad412184e6fa0ce7
unknown <ismail.ouali@ump.ac.ma>
2024-04-15 09:45:10 +0000

    Adding the layout

--- resources/views/Client/store.blade.php
+++ resources/views/Client/store.blade.php
@@ -70,46 +70,7 @@
     <body class="bg-black">
 
     <!--Nav-->
- 
-    <header class="">
-        <div class="object-top opacity-60 bg-gradient-to-r from-blue-500 bg-cover rounded">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="/images/logo.png" class="h-16" alt="Flowbite Logo" />
-                    <span class="self-center text-3xl text-white font-bold whitespace-nowrap">MoroccoExplorer</span>
-                </a>
-                <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button" class="text-white bg-amber-700 font-medium rounded-lg text-sm px-4 py-2 text-center">Get started</button>
-                    <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center bg-black p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden" aria-controls="navbar-cta" aria-expanded="false">
-                      <span class="sr-only">Open main menu</span>
-                      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                      </svg>
-                  </button>
-                </div>
-                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-                  <ul class="flex flex-col font-medium p-4 bg-transparent md:p-0 mt-4  rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
-                    <li>
-                      <a href="#" class="block py-2 px-3 md:p-0 text-white text-2xl rounded md:bg-transparent hover:text-blue-500 md:text-blue-700 md:dark:text-amber-500" aria-current="page">Home</a>
-                    </li>
-                    <li>
-                      <a href="#" class="block py-2 px-3 md:p-0 text-white text-2xl rounded hover:text-blue-500 hover:text-blue-500 md:hover:text-amber-700">Store</a>
-                    </li>
-                    <li>
-                      <a href="#" class="block py-2 px-3 md:p-0 text-white text-2xl rounded hover:text-blue-500 md:hover:text-amber-700">Travel</a>
-                    </li>
-                    <li>
-                      <a href="#" class="block py-2 px-3 md:p-0 text-white text-2xl rounded hover:text-blue-500 md:hover:text-amber-700">Blog</a>
-                    </li>
-                    <li>
-                      <a href="#" class="block py-2 px-3 md:p-0 text-white text-2xl rounded hover:text-blue-500 md:hover:text-amber-700">Contact</a>
-                    </li>
-                  </ul>
-                </div>
-            </div>                
-        </div>
-
-    </header>
+    @include('layouts.nav')
 
 
     <div class="carousel relative container mx-auto " style="max-width:1600px;">
@@ -318,23 +279,7 @@
 
  
 
-    <footer class="footer footer-center p-10 bg-gradient-to-r from-blue-500 opacity-50 text-white">
-        <div class="flex">
-
-            <aside>
-                <img class="w-1/4" src="/images/logo-big.png" alt="">                    
-
-                <p class="py-4">Copyright © 2024 - MoroccoExplorer</p>
-            </aside> 
-            <div class="justify-center align-center  gap-4 ">
-                <a href="" class="p-8 text-2xl font-semibold underline hover:text-white">Home</a>
-                <a href="" class="p-8 text-2xl font-semibold underline hover:text-white">Travels</a>
-                <a href="" class="p-8 text-2xl font-semibold underline hover:text-white">Blog</a>
-                <a href="" class="p-8 text-2xl font-semibold underline hover:text-white">Contact</a>
-                <a href="" class="p-8 text-2xl font-semibold underline hover:text-white">Store</a>
-            </div>
-        </div>
-    </footer>
+@include('layouts.footer')
 
 </body>
 
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="footer footer-center p-10 bg-gradient-to-r from-blue-500 opacity-50 text-white">
+    <div class="flex">
+
+        <aside>
+            <img class="w-1/4" src="/images/logo-big.png" alt="">                    
+
+            <p class="py-4">Copyright © 2024 - MoroccoExplorer</p>
+        </aside> 
+        <div class="justify-center align-center  gap-4 ">
+            <a href="" class="p-8 text-2xl font-semibold underline hover:text-white">Home</a>
+            <a href="" class="p-8 text-2xl font-semibold underline hover:text-white">Travels</a>
+            <a href="" class="p-8 text-2xl font-semibold underline hover:text-white">Blog</a>
+            <a href="" class="p-8 text-2xl font-semibold underline hover:text-white">Contact</a>
+            <a href="" class="p-8 text-2xl font-semibold underline hover:text-white">Store</a>
+        </div>
+    </div>
+</footer>

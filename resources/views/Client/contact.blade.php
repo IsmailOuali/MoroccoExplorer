f73736d70501720be721db9d44a1a67bf948652b
unknown <ismail.ouali@ump.ac.ma>
2024-04-15 11:22:25 +0000

    Fixing

--- resources/views/Client/contact.blade.php
+++ resources/views/Client/contact.blade.php
@@ -12,8 +12,8 @@
         
         @include('layouts.nav')
 
-        <section class="min-h-screen bg-cover " style="background-image: url('https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')">
-            <div class="flex flex-col min-h-screen bg-black/60">
+        <section class="min-h-screen bg-cover " style="background-image: url('https://images.pexels.com/photos/774835/pexels-photo-774835.jpeg?auto=compress&cs=tinysrgb&w=600')">
+            <div class="flex flex-col min-h-screen bg-black/50">
                 <div class="container flex flex-col flex-1 px-6 py-12 mx-auto">
                     <div class="flex-1 lg:flex lg:items-center lg:-mx-6">
         
@@ -29,17 +29,17 @@
                                 <form class="mt-6">
                                     <div class="flex-1">
                                         <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
-                                        <input type="text" placeholder="John Doe" class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                        <input type="text" placeholder="John Doe" class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md " />
                                     </div>
         
                                     <div class="flex-1 mt-6">
                                         <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
-                                        <input type="email" placeholder="johndoe@example.com" class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                        <input type="email" placeholder="johndoe@example.com" class="block w-full px-5 py-3 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md " />
                                     </div>
         
                                     <div class="w-full mt-6">
                                         <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
-                                        <textarea class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" placeholder="Message"></textarea>
+                                        <textarea class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 " placeholder="Message"></textarea>
                                     </div>
         
                                     <button class="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50">

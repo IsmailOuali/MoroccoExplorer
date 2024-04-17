4c3c35db6434a016c4463d6404dee9dc750e04ef
unknown <ismail.ouali@ump.ac.ma>
2024-04-17 00:34:23 +0000

    Adding all the database

---
+++ app/Http/Controllers/CategoryVoyageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CategoryVoyage;
+use App\Http\Requests\StoreCategoryVoyageRequest;
+use App\Http\Requests\UpdateCategoryVoyageRequest;
+
+class CategoryVoyageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryVoyageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CategoryVoyage $categoryVoyage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CategoryVoyage $categoryVoyage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryVoyageRequest $request, CategoryVoyage $categoryVoyage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CategoryVoyage $categoryVoyage)
+    {
+        //
+    }
+}
